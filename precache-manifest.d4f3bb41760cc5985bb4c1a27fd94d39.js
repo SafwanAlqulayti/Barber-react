78d40b81c444c183ec8e4573370ee74a03320b9b
صفوان القليطي <safwanalqulayti@gmail.com>
2020-01-16 09:34:59 +0300

    Updates

---
+++ precache-manifest.d4f3bb41760cc5985bb4c1a27fd94d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4c1f3d0035bc5cc76cc88f610b4e2f",
+    "url": "/Barber-react/index.html"
+  },
+  {
+    "revision": "2dcf11bb96be77fccd94",
+    "url": "/Barber-react/static/css/main.a10a3fda.chunk.css"
+  },
+  {
+    "revision": "3aa841d3a8a23f622da2",
+    "url": "/Barber-react/static/js/2.6ae16e8f.chunk.js"
+  },
+  {
+    "revision": "deebb561a20bbe7470d6175f1fa99411",
+    "url": "/Barber-react/static/js/2.6ae16e8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dcf11bb96be77fccd94",
+    "url": "/Barber-react/static/js/main.081551a0.chunk.js"
+  },
+  {
+    "revision": "3e2a9a8ec163f87d4b1a",
+    "url": "/Barber-react/static/js/runtime-main.f3e97b96.js"
+  },
+  {
+    "revision": "812d5d4bac922a4db2a9c6f4b3bfdb4b",
+    "url": "/Barber-react/static/media/new.812d5d4b.jpg"
+  }
+]);

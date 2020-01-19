d0429caa8569a74ff25a8be3a077517f72797a35
صفوان القليطي <safwanalqulayti@gmail.com>
2020-01-20 00:45:57 +0300

    product photo modified

--- barber/src/ProductInfo.js
+++ barber/src/ProductInfo.js
@@ -3,20 +3,20 @@ import './App.css';
 
 class ProductInfo extends Component {
     render() {
-      const style = {
-          width: "200px",
-          height: "200px",
-          border: "border:1px; border-style:solid; border-color:#FFf000;padding:10px" 
-      }
- 
+        const style = {
+            width: '200px',
+            height: '200px' 
+             
+          }
         return (
            
-                <div className="col-md-4"  style={{style}}>
-              
-           <img src={this.props.img} alt=""   ></img>     
+                <div className="ProductInfo" >
+              <div id="Product12">
+           <img src={this.props.img} alt=""  style={{style}}  ></img>     
            <p>{this.props.name}</p>
            <p>{this.props.description}</p>
            <p>{this.props.price}</p>
+           </div>
             </div>
      
         );
